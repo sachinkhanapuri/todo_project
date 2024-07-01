20422a20fe132a0c3e4ca008dbbd68fd5a5000a7
sachinkhanapuri <93sachinkhanapuri@gmail.com>
2024-07-01 18:22:44 +0100

    changes done into settings.py file

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIR =  'todo_app/static/'
-\
+
 STATIC_ROOT='static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

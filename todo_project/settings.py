b6c2c6bc802c309047cdd17e89c34cb135fd76a2
sachinkhanapuri <93sachinkhanapuri@gmail.com>
2024-07-01 18:22:17 +0100

    changes done into settings.py file

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
@@ -119,7 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIR =  'todo_app/static/'
-
+\
+STATIC_ROOT='static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
